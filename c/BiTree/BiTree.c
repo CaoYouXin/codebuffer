8e3b87fbd8e8a7226a0b1d1dde80b13870379cf9
曹又心 <c_youxin@163.com>
2014-10-04 20:21:51 +0800

    闹心，一个Project中不能有多个main函数。话说有木有单元测试可以用啊？！？

--- c/BiTree/BiTree.c
+++ c/BiTree/BiTree.c
@@ -11,43 +11,13 @@
 #define OK 1
 #define ERROR 0
 
-typedef int Status;
-typedef int ElemType;
-typedef struct Triplet {
-	ElemType e1;
-	ElemType e2;
-	ElemType e3;
-} *pTriplet;
 
-Status InitTriplet(pTriplet *pT, ElemType v1, ElemType v2, ElemType v3) {
-	(*pT) = (pTriplet) malloc(sizeof(struct Triplet));
-	if (!(*pT)) return ERROR;
-	(*(*pT)).e1 = v1;
-	(*(*pT)).e2 = v2;
-	(*(*pT)).e3 = v3;
-	return OK;
-}
 
-Status DestroyTriplet(pTriplet *T) {
-	free(*T);
-	*T = NULL;
-	return OK;
-}
 
-int main() {
-	puts("Let's start...");
-	pTriplet T = NULL;
-	Status s = InitTriplet(&T, 1, 2, 3);
-	if (s) {
-		printf(" %d %d %d \n", (*T).e1, (*T).e2, (*T).e3);
-	} else {
-		puts("Initial Error!");
-		return 0;
-	}
-	do {
-		s = DestroyTriplet(&T);
-	} while (!s);
-	puts("That's all!");
-	return 0;
-}
+//int main() {
+//	puts("Let's start...");
+
+//	puts("That's all!");
+//	return 0;
+//}
 
---
+++ c/Triplet/Triplet.c
@@ -0,0 +1,52 @@
+/*
+ * Triplet.c
+ *
+ *  Created on: 2014年9月26日
+ *      Author: caolisheng
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#define OK 1
+#define ERROR 0
+
+typedef int Status;
+typedef int ElemType;
+typedef struct Triplet {
+	ElemType e1;
+	ElemType e2;
+	ElemType e3;
+} *pTriplet;
+
+Status InitTriplet(pTriplet *pT, ElemType v1, ElemType v2, ElemType v3) {
+	(*pT) = (pTriplet) malloc(sizeof(struct Triplet));
+	if (!(*pT)) return ERROR;
+	(*(*pT)).e1 = v1;
+	(*(*pT)).e2 = v2;
+	(*(*pT)).e3 = v3;
+	return OK;
+}
+
+Status DestroyTriplet(pTriplet *T) {
+	free(*T);
+	*T = NULL;
+	return OK;
+}
+
+int main() {
+	puts("Let's start...");
+	pTriplet T = NULL;
+	Status s = InitTriplet(&T, 1, 2, 3);
+	if (s) {
+		printf(" %d %d %d \n", (*T).e1, (*T).e2, (*T).e3);
+	} else {
+		puts("Initial Error!");
+		return 0;
+	}
+	do {
+		s = DestroyTriplet(&T);
+	} while (!s);
+	puts("That's all!");
+	return 0;
+}
