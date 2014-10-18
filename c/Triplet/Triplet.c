9dd20b07d92b06af5503b95ab0b42dab626fcb09
曹又心 <c_youxin@163.com>
2014-10-18 14:28:20 +0800

    5年没写过C代码，oh，god，交换两个数纠结了好久

--- c/Triplet/Triplet.c
+++ c/Triplet/Triplet.c
@@ -18,12 +18,27 @@ typedef struct Triplet {
 	ElemType e3;
 } *pTriplet;
 
-Status InitTriplet(pTriplet *pT, ElemType v1, ElemType v2, ElemType v3) {
+void swap(int *i1, int *i2, int *tmp) {
+	if (*i1 < *i2) {
+		*tmp = *i1;
+		*i1 = *i2;
+		*i2 = *tmp;
+	}
+}
+
+Status InitTriplet(pTriplet *pT) {
+	int v1, v2, v3, tmp;
+	scanf("%d %d %d", &v1, &v2, &v3);
+
+	swap(&v1, &v2, &tmp);
+	swap(&v1, &v2, &tmp);
+	swap(&v2, &v3, &tmp);
+
 	(*pT) = (pTriplet) malloc(sizeof(struct Triplet));
 	if (!(*pT)) return ERROR;
-	(*(*pT)).e1 = v1;
-	(*(*pT)).e2 = v2;
-	(*(*pT)).e3 = v3;
+	(*pT)->e1 = v1;
+	(*pT)->e2 = v2;
+	(*pT)->e3 = v3;
 	return OK;
 }
 
@@ -36,7 +51,7 @@ Status DestroyTriplet(pTriplet *T) {
 //int main() {
 //	puts("Let's start...");
 //	pTriplet T = NULL;
-//	Status s = InitTriplet(&T, 1, 2, 3);
+//	Status s = InitTriplet(&T);
 //	if (s) {
 //		printf(" %d %d %d \n", T->e1, T->e2, T->e3);
 //	} else {
